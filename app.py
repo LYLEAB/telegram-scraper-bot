ab4eb8a61355e903d527df8e15a4abb01479dd32
LY_LEAB <fiiyuu096@gmail.com>
2026-04-06 12:54:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -78,4 +78,6 @@ def handle_webhook():
     return jsonify({"status": "success", "kobo_id": kobo_id}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    # Railway provides the PORT variable, we must use it
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
