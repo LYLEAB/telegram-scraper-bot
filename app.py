5cf8f95e2519797e6bd305eef009da426dd1ce29
LY_LEAB <fiiyuu096@gmail.com>
2026-04-20 17:14:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,17 @@ except gspread.exceptions.WorksheetNotFound:
     raw_sheet.append_row(["Date", "Kobo ID", "Full Raw Code"])
 
 # --- DICTIONARIES FOR KOBO DATA TRANSLATION ---
+CATEGORY_MAP = {
+    "beer": "Beer",
+    "ed": "ED",
+    "med": "MED",
+    "csd": "CSD",
+    "scsd": "SCSD",
+    "isotonic": "Isotonic",
+    "rtd_tea": "RTD Tea",
+    "water": "Water"
+}
+
 PROVINCE_MAP = {
     "bmc": "Banteay Meanchey", "btt": "Battambang", "kc": "Kampong Cham", 
     "kchh": "Kampong Chhnang", "kspe": "Kampong Speu", "ktho": "Kampong Thom", 
@@ -128,15 +139,69 @@ CHANNEL_MAP = {
     "wedding": "Wedding"
 }
 
-CATEGORY_MAP = {
-    "beer": "Beer",
-    "ed": "ED",
-    "med": "MED",
-    "csd": "CSD",
-    "scsd": "SCSD",
-    "isotonic": "Isotonic",
-    "rtd_tea": "RTD Tea",
-    "water": "Water"
+BRAND_MAP = {
+    "beer_abc_can_330ml": "ABC Can 330ml", "beer_abc_pint_330ml": "ABC Pint 330ml", "beer_abc_smooth_stout_can_330ml": "ABC Smooth Stout Can 330ml",
+    "beer_anchor_can_330ml": "Anchor Can 330ml", "beer_anchor_pint_330ml": "Anchor Pint 330ml", "beer_anchor_white_pint_330ml": "Anchor White Pint 330ml",
+    "beer_anchor_white_can_330ml": "Anchor White Can 330ml", "beer_anchor_ultra_can_330ml": "Anchor Ultra Can 330ml", "beer_angkor_can_330ml": "Angkor Can 330ml",
+    "beer_angkor_pint_330ml": "Angkor Pint 330ml", "beer_angkor_sky_can_330ml": "Angkor Sky Can 330ml", "beer_barrley_black_can_330ml": "Barrley Black Can 330ml",
+    "beer_black_panther_can_330ml": "Black Panther Can 330ml", "beer_cambodia_can_330ml": "Cambodia Beer Can 330ml", "beer_cambodia_pint_330ml": "Cambodia Beer Pint 330ml",
+    "beer_cambodia_lite_can_330ml": "Cambodia Lite Beer Can 330ml", "beer_cambodia_lite_pint_330ml": "Cambodia Lite Beer Pint 330ml", "beer_cambodia_premium_draft_pint_330ml": "Cambodia Premium Draft Beer Pint 330ml",
+    "beer_cambodia_black_can_330ml": "Cambodia Black Beer Can 330ml", "beer_cambodia_black_pint_330ml": "Cambodia Black Beer Pint 330ml", "beer_chang_can_330ml": "Chang Can 330ml",
+    "beer_clasei_can_330ml": "Clasei Can 330ml", "beer_dragon_can_330ml": "Dragon Can 330ml", "beer_dragon_nitro_can_330ml": "Dragon Nitro Can 330ml",
+    "beer_dragon_nitro_pint_330ml": "Dragon Nitro Pint 330ml", "beer_dragon_silver_can_330ml": "Dragon Silver Can 330ml", "beer_ganzberg_can_330ml": "Ganzberg Can 330ml",
+    "beer_ganzberg_gold_lite_can_330ml": "Ganzberg Gold Lite Can 330ml", "beer_ganzberg_snow_can_330ml": "Ganzberg Snow Can 330ml", "beer_greet_can_330ml": "Greet Can 330ml",
+    "beer_greet_lite_can_330ml": "Greet Lite Can 330ml", "beer_guiness_can_330ml": "Guiness Can 330ml", "beer_hanuman_can_330ml": "Hanuman Can 330ml",
+    "beer_hanuman_pint_330ml": "Hanuman Pint 330ml", "beer_hanuman_lite_can_330ml": "Hanuman Lite Can 330ml", "beer_hanuman_black_can_330ml": "Hanuman Black Can 330ml",
+    "beer_hanuman_black_pint_330ml": "Hanuman Black Pint 330ml", "beer_krud_can_330ml": "Krud Can 330ml", "beer_krud_pint_330ml": "Krud Pint 330ml",
+    "beer_krud_lite_can_330ml": "Krud Lite Can 330ml", "beer_krud_lite_pint_330ml": "Krud Lite Pint 330ml", "beer_leo_can_330ml": "LEO Can 330ml",
+    "beer_mattrid_can_330ml": "Mattrid Can 330ml", "beer_neak_can_330ml": "Neak Beer Can 330ml", "beer_singha_can_330ml": "Singha Beer Can 330ml",
+    "beer_singha_pint_330ml": "Signha Beer Pint 330ml", "beer_tiger_can_330ml": "Tiger Can 330ml", "beer_tiger_pint_330ml": "Tiger Pint 330ml",
+    "beer_tiger_crystal_can_330ml": "Tiger Crystal Can 330ml", "beer_tiger_crystal_pint_330ml": "Tiger Crystal Pint 330ml", "beer_vattanac_can_330ml": "Vattanac Can 330ml",
+    "beer_vattanac_pint_330ml": "Vattanac Pint 330ml", "beer_vattanac_lagerade_can_330ml": "Vattanac Lagerade Can 330ml", "beer_vattanac_premium_light_can_330ml": "Vattanac Premium Light Can 330ml",
+    "beer_vattanac_premium_light_pint_330ml": "Vattanac Premium Light Pint 330ml", "beer_umai_mugi_can_330ml": "Umai Mugi Can 330ml", "csd_7up_can_330ml": "7up Can 33ml",
+    "csd_big_cola_pet_320ml": "Big Cola PET 320ml", "csd_big_cola_pet_1500ml": "Big Cola PET 1.5L", "csd_big_cola_pet_3100ml": "Big Cola PET 3.1L",
+    "csd_cocacola_can_185ml": "Coca-Cola Can 185ml", "csd_cocacola_can_330ml": "Coca-Cola Can 330ml", "csd_cocacola_pet_1500ml": "Coca-Cola PET 1.5L",
+    "csd_cocacola_pet_600ml": "Coca-Cola PET 600ml", "csd_cambodia_cola_can_250ml": "Cambodia Cola Can 250ml", "csd_cambodia_cola_can_330ml": "Cambodia Cola Can 330ml",
+    "csd_est_cola_pet_1500ml": "Est Cola PET 1.5L", "csd_est_cola_can_325ml": "Est Cola Can 325ml", "csd_fanta_sprite_can_330ml": "Fanta & Sprite Can 330ml",
+    "csd_ize_cola_can_330ml": "IZE Cola Can 330ml", "csd_ize_cola_can_250ml": "IZE Cola Can 250ml", "csd_ize_cola_pet_300ml": "IZE Cola PET 300ml",
+    "csd_ize_cola_pet_500ml": "IZE Cola PET 500ml", "csd_ize_cola_pet_1500ml": "IZE Cola PET 1.5L", "csd_krud_cola_can_250ml": "Krud Cola Can 250ml",
+    "csd_mirinda_green_can_330ml": "Mirinda Green Can 330ml", "csd_mirinda_orange_can_330ml": "Mirinda Orange Can 330ml", "csd_pepsi_can_330ml": "Pepsi Can 330ml",
+    "ed_bacchus_can_250ml": "Bacchus Can 250ml", "ed_boostrong_can_250ml": "Boostrong Can 250ml", "ed_super_boostrong_can_250ml": "Super Boostrong Can 250ml",
+    "ed_carabao_can_250ml": "Carabao Can 250ml", "ed_dazz_can_250ml": "DAZZ Can 250ml", "ed_dazz_zero_can_250ml": "DAZZ Zero Can 250ml",
+    "ed_greet_energy_can_250ml": "Greet Energy Can 250ml", "ed_krud_energy_can_250ml": "Krud Energy Can 250ml", "ed_krud_pure_charge_can_250ml": "Krud Pure Charge Can 250ml",
+    "ed_m150_can_250ml": "M-150 Can 250ml", "ed_m150_pint_150ml": "M-150 Pint 150ml", "ed_missile_can_250ml": "Missile Can 250ml",
+    "ed_powaram_can_330ml": "POWARAM Can 330ml", "ed_red_ant_can_250ml": "Red Ant Can 250ml", "ed_redbull_can_250ml": "Red-Bull Can 250ml",
+    "ed_vikingz_can_250ml": "Vikingz Can 250ml", "ed_wurkz_can_250ml": "Wurkz Can 250ml", "ed_dragon_can_250ml": "Dragon Can 250ml",
+    "ed_aira_can_250ml": "Aira Can 250ml", "ed_sting_power_can_250ml": "Sting Power Can 250ml", "ed_kingkong_can_250ml": "King Kong Can 250ml",
+    "isotonic_pocari_sweat_pet_500ml": "Pocari Sweat PET 500ml", "isotonic_vactive_white_pet_350ml": "V-Active White PET 350ml", "isotonic_vactive_white_pet_600ml": "V-Active White PET 600ml",
+    "isotonic_vactive_white_can_330ml": "V-Active White Can 330ml", "isotonic_vactive_green_can_330ml": "V-Active Green Can 330ml", "isotonic_vactive_red_can_330ml": "V-Active Red Can 330ml",
+    "isotonic_vactive_yellow_can_330ml": "V-Active Yellow Can 330ml", "isotonic_vactive_yellow_pet_350ml": "V-Active Yellow PET 350ml", "isotonic_vactive_yellow_pet_600ml": "V-Active Yellow PET 600ml",
+    "isotonic_cambodia_sport_pet_300ml": "Cambodia Sport+ PET 300ml", "isotonic_cambodia_sport_pet_500ml": "Cambodia Sport+ PET 500ml", "med_champion_can_250ml": "Champion Can 250ml",
+    "med_champion_green_can_250ml": "Champion Green Can 250ml", "med_icy_cool_can_250ml": "Icy Cool Can 250ml", "med_kizz_can_250ml": "KIZZ Can 250ml",
+    "med_krud_ice_power_can_250ml": "Krud Ice Power Can 250ml", "med_vigor_can_250ml": "Vigor Can 250ml", "med_wurkz_ice_can_250ml": "Wurkz ICE Can 250ml",
+    "rtd_tea_ichitan_pet_500ml": "Ichitan PET 500ml", "rtd_tea_oishi_pet_500ml": "Oishi PET 500ml", "rtd_tea_ad_pet_500ml": "AD PET 500ml",
+    "rtd_tea_v_lemon_pet_500ml": "V Lemon PET 500ml", "scsd_exprez_strawberry_can_330ml": "Exprez Strawberry Can 330ml", "scsd_exprez_strawberry_pet_300ml": "Exprez Strawberry PET 300ml",
+    "scsd_exprez_strawberry_pet_500ml": "Exprez Strawberry PET 500ml", "scsd_exprez_melon_can_330ml": "Exprez Melon Can 330ml", "scsd_exprez_melon_pet_300ml": "Exprez Melon PET 300ml",
+    "scsd_idol_can_330ml": "Idol Can 330ml", "scsd_samurai_can_330ml": "Samurai Can 330ml", "scsd_samurai_pet_480ml": "Samurai PET 480ml",
+    "scsd_sting_red_can_330ml": "Sting Red Can 330ml", "scsd_sting_red_pet_500ml": "Sting Red PET 500ml", "scsd_sting_yellow_can_330ml": "Sting Yellow Can 330ml",
+    "scsd_sting_yellow_pet_500ml": "Sting Yellow PET 500ml", "scsd_sting_blue_charge_can_330ml": "Sting Blue Can 330ml", "scsd_top_upp_can_330ml": "Top Upp Can 330ml",
+    "water_angkor_puro_pet_350ml": "Angkor Puro PET 350ml", "water_angkor_puro_pet_500ml": "Angkor Puro PET 500ml", "water_angkor_puro_pet_1500ml": "Angkor Puro PET 1.5L",
+    "water_aruna_pet_500ml": "Aruna PET 500ml", "water_aruna_pet_1500ml": "Aruna PET 1.5L", "water_bn_pet_630ml": "BN Water PET 630ml",
+    "water_bn_pet_1500ml": "BN Water PET 1.5L", "water_cambodia_pet_350ml": "Cambodia Water PET 350ml", "water_cambodia_pet_500ml": "Cambodia Water PET 500ml",
+    "water_cambodia_pet_1500ml": "Cambodia Water PET 1.5L", "water_colee_pet_300ml": "Colee Water PET 300ml", "water_colee_pet_500ml": "Colee Water PET 500ml",
+    "water_colee_pet_1500ml": "Colee Water PET 1.5L", "water_dasani_pet_500ml": "Dasani PET 500ml", "water_dasani_pet_1500ml": "Dasani PET 1.5L",
+    "water_hitech_pet_250ml": "Hi-Tech PET 250ml", "water_hitech_pet_350ml": "Hi-Tech PET 350ml", "water_hitech_pet_600ml": "Hi-Tech PET 600ml",
+    "water_hitech_pet_1500ml": "Hi-Tech PET 1.5L", "water_lyyon_pet_500ml": "Lyyon PET 500ml", "water_lyyon_pet_1500ml": "Lyyon PET 1.5L",
+    "water_namthip_pet_550ml": "Namthip PET 550ml", "water_oral_pet_500ml": "Oral PET 500ml", "water_provida_pet_250ml": "Provida PET 250ml",
+    "water_provida_pet_330ml": "Provida PET 330ml", "water_provida_pet_500ml": "Provida PET 500ml", "water_provida_pet_1500ml": "Provida PET 1.5L",
+    "water_singha_pet_600ml": "Singha Water PET 600ml", "water_singha_pet_1500ml": "Singha Water PET 1.5L", "water_vital_pet_250ml": "Vital PET 250ml",
+    "water_vital_pet_350ml": "Vital PET 350ml", "water_vital_pet_500ml": "Vital PET 500ml", "water_vital_pet_1500ml": "Vital PET 1.5L",
+    "water_eragold_pet_300ml": "Eragold PET 300ml", "water_eragold_pet_500ml": "Eragold PET 500ml", "water_eragold_pet_1500ml": "Eragold PET 1500ml",
+    "water_bokor_pet_330ml": "Bokor PET 330ml", "water_bokor_pet_500ml": "Bokor PET 500ml", "water_bokor_pet_1500ml": "Bokor PET 1500ml",
+    "water_bokor_pet_2000ml": "Bokor PET 2000ml", "water_ganzberg_pet_500ml": "Ganzberg PET 500ml", "water_ganzberg_pet_1500ml": "Ganzberg PET 1500ml",
+    "water_kulen_pet_330ml": "Kulen PET 330ml", "water_kulen_pet_500ml": "Kulen PET 500ml", "water_kulen_pet_700ml": "Kulen PET 700ml",
+    "water_kulen_pet_1500ml": "Kulen PET 1500ml", "water_kulen_pet_2000ml": "Kulen PET 2000ml", "water_elan_pet_300ml": "Elan PET 300ml",
+    "water_elan_pet_500ml": "Elan PET 500ml", "water_elan_pet_1500ml": "Elan PET 1500ml"
 }
 
 # --- HELPERS ---
@@ -171,26 +236,6 @@ def to_number(val):
         return float(clean_val) if '.' in clean_val else int(clean_val)
     except ValueError:
         return clean_val
-
-def format_packaging(pack_str):
-    """Formats packaging case correctly and converts ml >= 1000 to Liters."""
-    if not pack_str:
-        return ""
-    
-    # Standardize spelling/casing of container types
-    pack_str = pack_str.title().replace('Pet', 'PET').replace('Ml', 'ml')
-    
-    # Find numbers followed by 'ml' to convert Liters
-    match = re.search(r'([\d\.]+)\s*ml', pack_str, re.IGNORECASE)
-    if match:
-        ml_val = float(match.group(1))
-        if ml_val >= 1000:
-            l_val = ml_val / 1000
-            # format as :g to smartly drop zeros (e.g., 1.500 -> 1.5)
-            l_str = f"{l_val:g}L"
-            pack_str = re.sub(r'[\d\.]+\s*ml', l_str, pack_str, flags=re.IGNORECASE)
-            
-    return pack_str
 
 def clean_html(text):
     if text is None:
@@ -257,9 +302,9 @@ def handle_webhook():
     region = str(data.get('region_select') or data.get('region') or 'N/A').upper()
     dealer = str(data.get('dealer_select') or 'N/A').upper()
     
-    # --- Exact Category Match ---
+    # --- Category mapped exactly to Kobo form label ---
     cat_raw = str(data.get('category') or '').lower().strip()
-    category = CATEGORY_MAP.get(cat_raw, cat_raw.title())
+    category = CATEGORY_MAP.get(cat_raw, cat_raw.title().replace('_', ' '))
     
     type_val = str(data.get('type_select') or 'N/A').upper()
     note = str(data.get('note_remark') or '')
@@ -297,26 +342,27 @@ def handle_webhook():
     f_prod = to_number(scheme_parts[1] if len(scheme_parts) > 1 else "")
     posm = "+".join(scheme_parts[2:]) if len(scheme_parts) > 2 else ""
 
-    # --- BRAND FORMATTING (WITH PREFIX STRIPPER) ---
+    # --- BRAND & PACKAGING FORMATTING ---
     brand_raw = str(data.get('brand_select') or 'Unknown Brand')
     
-    prefixes_to_remove = ["beer_", "csd_", "ed_", "isotonic_", "med_", "rtd_tea_", "scsd_", "water_"]
-    for p in prefixes_to_remove:
-        if brand_raw.startswith(p):
-            brand_raw = brand_raw[len(p):]
-            break
+    # 1. Map directly to exact Kobo Form Label
+    brand_clean = BRAND_MAP.get(brand_raw, brand_raw.replace('_', ' ').title())
+    
+    # 2. Convert 1000ml or higher to Liters across ANY brand smoothly
+    match = re.search(r'([\d\.]+)\s*ml', brand_clean, re.IGNORECASE)
+    if match:
+        ml_val = float(match.group(1))
+        if ml_val >= 1000:
+            l_val = ml_val / 1000
+            l_str = f"{l_val:g}L"
+            brand_clean = re.sub(r'[\d\.]+\s*ml', l_str, brand_clean, flags=re.IGNORECASE)
 
-    brand_clean = brand_raw.replace('_', ' ').title()
-    
-    for prefix in ["Ed ", "Csd ", "Med ", "Rtd ", "Scsd ", "Abc "]:
-        if prefix in brand_clean:
-            brand_clean = brand_clean.replace(prefix, prefix.upper())
-            
-    brand_clean = brand_clean.replace('Ml', 'ml')
-    
-    # --- Intelligent Packaging Extraction ---
-    pack_match = re.search(r'(Can|Pint|PET|Pet|Bottle)[\s_]*[\d\.]+[a-zA-Z]+', brand_clean, re.IGNORECASE)
-    packaging = format_packaging(pack_match.group(0)) if pack_match else ""
+    # 3. Clean up generic caps in case of unmapped items
+    brand_clean = brand_clean.replace('Pet', 'PET').replace('Ml', 'ml')
+
+    # 4. Extract pure packaging to its own column cleanly (e.g., "PET 1.5L" or "Can 330ml")
+    pack_match = re.search(r'(Can|Pint|PET|Bottle)[\s_]*[\d\.]+[a-zA-Z]+', brand_clean, re.IGNORECASE)
+    packaging = pack_match.group(0) if pack_match else ""
     
     brand_final = f"{brand_clean}-{type_val}" if type_val and type_val != 'N/A' else brand_clean
     week_val = str(data.get('week_num') or '').replace('week', 'Week ')
